0a098eebcbbac33864276932d500e16e031ac329
Utsab Saha <utsabsaha@Utsabs-MacBook-Pro.local>
2025-02-11 17:31:25 -0800

    Boilerplate testing code for one test.  Note: this is unfinished

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,5 +1,35 @@
-describe('Example Test', () => {
-  it('should pass', () => {
-    expect(true).toBe(false);
+/**
+ * @jest-environment node
+ */
+
+import request from 'supertest';
+import { createServer } from 'http';
+import { auth } from '@/auth';
+import { GET } from '@/app/api/applications/route';
+
+
+jest.mock('@/auth', () => ({
+  auth: jest.fn(),
+}));
+
+describe('GET /api/applications', () => {
+  it('should return only applications belonging to the logged-in user', async () => {
+    (auth as jest.Mock).mockResolvedValue({
+      user: {
+        email: 'utsab.k.saha@gmail.com',
+      },
+    });
+
+    // const requestObj = {
+    //   nextUrl: {
+    //     searchParams: new URLSearchParams({ Id: '1' }),
+    //   },
+    // } as any;
+
+
+    const response = await GET();
+    const body = await response.json();
+
+    expect(response.status).toBe(200);
   });
 });
