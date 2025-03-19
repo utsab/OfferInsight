0009dd024c04dc0f8d6e7371b07496cc210445b0
ttran913 <122418320+ttran913@users.noreply.github.com>
2025-03-19 11:53:39 +0000

    Made 'Continue' button a real looking button. Removed header and footer in layout for cleaner look.

--- app/onboarding/layout.tsx
+++ app/onboarding/layout.tsx
@@ -3,27 +3,30 @@ import Link from 'next/link';
 
 export default function OnboardingLayout({ children }: { children: ReactNode }) {
   return (
+    // <div>
+    //   <header>
+    //     <h1>Onboarding Process</h1>
+    //   </header>
+    //   <main>{children}</main>
+    //   <footer>
+    //     <nav>
+    //       <ul>
+    //         <li>
+    //           <Link href="/onboarding">
+    //             Go to Onboarding
+    //           </Link>
+    //         </li>
+    //         <li>
+    //           <Link href="/">
+    //             Go to Homepage
+    //           </Link>
+    //         </li>
+    //       </ul>
+    //     </nav>
+    //   </footer>
+    // </div>
     <div>
-      <header>
-        <h1>Onboarding Process</h1>
-      </header>
       <main>{children}</main>
-      <footer>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/onboarding">
-                Go to Onboarding
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                Go to Homepage
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </footer>
     </div>
   );
 }
