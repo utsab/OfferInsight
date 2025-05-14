535cc3feebfb97510957ae9c73302e282bc6a3fb
Brendan Sick <brendansick@gmail.com>
2025-05-14 16:45:27 -0700

    reduced header(navbar) size

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { UserButton } from './user-button';
-import './navbar.css';
+import Image from "next/image";
+import Link from "next/link";
+import { UserButton } from "./user-button";
+import "./navbar.css";
 
 export function Navbar() {
   return (
@@ -9,12 +9,13 @@ export function Navbar() {
       <div className="navbar-container">
         <div className="logo-container">
           <Link href="/">
-            <Image 
-              src="/images/logo-dark.png" 
-              alt="Company Logo" 
-              width={300} 
-              height={100} 
+            <Image
+              src="/images/logo-dark.png"
+              alt="Company Logo"
+              width={200}
+              height={50}
               priority
+              style={{ height: "auto" }}
             />
           </Link>
         </div>
@@ -22,4 +23,4 @@ export function Navbar() {
       </div>
     </nav>
   );
-} 
+}
