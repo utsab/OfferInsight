ca9eb0252e26c8b636226388298527723501d86f
Tuan Tran <ttran913@gmail.com>
2025-12-21 02:58:21 -0800

    Syncing schema.prisma with new model without migrations, instead using db push.

--- prisma/migrations/20251218165833_rename_dateCompleted_to_dateModified/migration.sql
+++
@@ -1,6 +0,0 @@
--- Rename dateCompleted to dateModified in all tables
-
-ALTER TABLE "Applications_With_Outreach" RENAME COLUMN "dateCompleted" TO "dateModified";
-ALTER TABLE "Linkedin_Outreach" RENAME COLUMN "dateCompleted" TO "dateModified";
-ALTER TABLE "In_Person_Events" RENAME COLUMN "dateCompleted" TO "dateModified";
-ALTER TABLE "Leetcode_Practice" RENAME COLUMN "dateCompleted" TO "dateModified";
