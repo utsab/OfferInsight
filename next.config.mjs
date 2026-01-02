28c9799deab2b492accc7c1bd7801e64416ad5df
Tuan Tran <ttran913@gmail.com>
2026-01-02 04:59:41 -0800

    Making changes here to access site with a phone.

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
     serverActions: {
       allowedOrigins: [
         'localhost:3000', // localhost
+        '192.168.1.47:3000', // Phone access via WiFi
         'https://scaling-memory-pj7rj746pv929rqg-3000.app.github.dev/', // Codespaces
       ],
     },
