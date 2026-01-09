405293f643a28c02c02ce0bcc0cb9a7f7ce988c9
Tuan Tran <ttran913@gmail.com>
2026-01-09 04:15:43 -0800

    Just simple settings fix for accessing the site from wifi ip

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,16 @@ const nextConfig = {
     serverActions: {
       allowedOrigins: [
         'localhost:3000', // localhost
-        '192.168.1.47:3000', // Phone access via WiFi
+        '192.168.1.52:3000', // Phone access via WiFi
         'https://scaling-memory-pj7rj746pv929rqg-3000.app.github.dev/', // Codespaces
       ],
     },
   },
+  allowedDevOrigins: [
+    'localhost:3000',
+    '192.168.1.52',
+    '192.168.1.52:3000',
+  ],
 };
 
 export default nextConfig;
