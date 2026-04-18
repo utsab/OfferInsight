4564432d13cffd5709e2fcc13afe1c817626a02c
Tuan Tran <ttran913@gmail.com>
2026-04-18 05:53:08 -0700

    Fixing node version mismatch

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
+/** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.jest.json',
+      },
+    ],
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1', // Adjust this based on your path aliases
     '^auth$': '<rootDir>/auth.ts', // Resolve bare 'auth' specifier used in route handlers
